703aad4a6b80017515750b817d6ef1d23a3d8b24
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-22 15:37:12 -0300

    definindo o menu customizado

---
+++ website/src/components/Menu/Menu.js
@@ -0,0 +1,46 @@
+import { Menu } from "antd"
+import React from "react"
+
+function CustomMenu () {
+
+    const items = [
+        {
+          label: 'Página Inicial',
+          key: 'home',
+        },
+        {
+          label: 'Sobre a Instituição',
+          key: 'about',
+        },
+        {
+            label: 'Programas Acadêmicos',
+            key: 'academic-programs'
+        },
+        {
+            label: 'Admissões',
+            key: 'admissions'
+        },
+        {
+            label: 'Vida Estudantil',
+            key: 'life-student'
+        },
+        {
+            label: 'Pesquisa e Inovação',
+            key: 'research-and-innovation'
+        },
+        {
+            label: 'Notícias', 
+            key: 'news',
+        },
+        {
+            label: 'Contato',
+            key: 'contact'
+        }
+    ]
+    return (
+        <Menu theme="dark" style={{backgroundColor: "var(--primary-color)"}} items={items} mode="horizontal" />
+    )
+    
+}
+
+export default CustomMenu
