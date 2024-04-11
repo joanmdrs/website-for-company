6842628b884624f63e6bc17a0faebd19d31c09b0
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-10 22:03:10 -0300

    refatorando a seção de razões

--- website/src/pages/Reasons/Reasons.js
+++ website/src/pages/Reasons/Reasons.js
@@ -1,61 +1,35 @@
-import "./Reasons.css"
-import assetAcademicExcelence from "../../assets/reasons/academic-excellence.png"
-import assetProfessionalDevelopment from "../../assets/reasons/professional-development.png"
-import assetIntegratedCommunity from "../../assets/reasons/integrated-community.png"
+import "./Reasons.css";
+import Image from "next/image";
+import { motivos } from "./data/dataReasons";
 
-import Image from "next/image"
-
-function Reasons(){
-    const motivos = [
-        {   
-            image: <Image src={assetAcademicExcelence} width={100} />,
-            title: "Excelência Acadêmica",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-            image: <Image src={assetProfessionalDevelopment} width={100}/>,
-            title: "Desenvolvimento Profissional",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-            image: <Image src={assetIntegratedCommunity} width={100}/>,
-            title: "Comunidade integrada",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-        }
-    ]
+function Reasons() {
     return (
-        <section className="reasons-section"> 
-            <div className="reasons-title"> 
-                <h2> POR QUÊ <span>VOCÊ</span> DEVE NOS ESCOLHER ? </h2>
+        <section className="reasons-section">
+            <div className="reasons-title">
+                <h2>
+                    POR QUÊ <span>VOCÊ</span> DEVE NOS ESCOLHER ?
+                </h2>
+                <p> 
+                    Em nossa instituição, estamos comprometidos em proporcionar uma experiência educacional excepcional que vai além das salas de aula. 
+                </p>
             </div>
-          
-            <div className="reasons-content"> 
-                {
-                    motivos.map((motivo, index) => (
-                        <div key={index} className="item-content">   
-                            <div>
-                                {motivo.image}
-                            </div>
-                            <div className="item-title">
-                                <h3> {motivo.title} </h3>
-                            </div>
 
-                            <div className="item-description">
-                                <p> {motivo.description} </p>
-                            </div>
-                            <div class="border-top"></div>
-                            <div class="border-bottom"></div>
-                            <div class="border-left"></div>
-                            <div class="border-right"></div>
-
-
+            <div className="reasons-content">
+                {motivos.map((motivo, index) => (
+                    <div key={index} className="item-content">
+                        <div className="item-img">{motivo.image}</div>
+                        <div className="item-title">
+                            <h3> {motivo.title} </h3>
                         </div>
-                    ))
-                }
+
+                        <div className="item-description">
+                            <p> {motivo.description} </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
-    )
-    
+    );
 }
 
-export default Reasons 
+export default Reasons;
---
+++ website/src/pages/Reasons/data/dataReasons.js
@@ -0,0 +1,43 @@
+import asset1 from "../../../assets/reasons/excelência.png"
+import asset2 from "../../../assets/reasons/desenvolvimento.png"
+import asset3 from "../../../assets/reasons/comunidade.png"
+import asset4 from "../../../assets/reasons/inovacao.png"
+import asset5 from "../../../assets/reasons/responsabilidade.png"
+import asset6 from "../../../assets/reasons/empreendedorismo.png"
+import Image from "next/image";
+
+export const motivos = [
+    {   
+        image: <Image src={asset1}/>,
+        title: "Excelência Acadêmica",
+        description:
+            "Nossa instituição se dedica incansavelmente à busca da excelência acadêmica. Desde a seleção cuidadosa de professores altamente qualificados até o fornecimento de recursos educacionais de última geração. ",
+    },
+    {
+        image: <Image src={asset2}/>,
+        title: "Desenvolvimento Profissional",
+        description:
+            "Nosso compromisso com o desenvolvimento profissional vai além da sala de aula. Oferecemos uma ampla gama de oportunidades e recursos para ajudar nossos alunos a desenvolverem suas habilidades e avançarem em suas carreiras. ",
+    },
+    {
+        image: <Image src={asset3}/>,
+        title: "Comunidade integrada",
+        description:
+            "Nossa comunidade é mais do que um simples grupo de pessoas. É um ambiente acolhedor e inclusivo, onde cada membro é valorizado e respeitado. "
+    },
+    {
+        image: <Image src={asset4}/>,
+        title: "Inovação Tecnológica",
+        description: "Na vanguarda da inovação tecnológica, estamos comprometidos em fornecer uma experiência educacional que integra as últimas tendências e avanços tecnológicos. "
+    },
+    {
+        image: <Image src={asset5}/>,
+        title: "Responsabilidade Social e Ambiental",
+        description: "Acreditamos que a educação vai além das salas de aula e dos currículos acadêmicos. É também sobre responsabilidade social e ambiental. "
+    },
+    {
+        image: <Image src={asset6}/>,
+        title: "Empreendedorismo e Inovação",
+        description: "Fomentamos uma cultura de empreendedorismo e inovação, onde a criatividade e a ambição são incentivadas e valorizadas. "
+    }
+];
