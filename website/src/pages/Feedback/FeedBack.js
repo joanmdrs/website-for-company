f09a8e7e07e3dd60b8f934fdef14800898ad8c4c
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-13 11:07:59 -0300

    refatorando a seção de depoimentos

--- website/src/pages/Feedback/FeedBack.js
+++ website/src/pages/Feedback/FeedBack.js
@@ -1,60 +1,71 @@
-import Image from "next/image"
+'use client'
 import "./FeedBack.css"
-import { BsYoutube } from "react-icons/bs";
-import student1 from "../../assets/feedbacks/student-1.jpg"
-import student2 from "../../assets/feedbacks/student-2.jpg"
-import student3 from "../../assets/feedbacks/student-3.jpg"
-import student4 from "../../assets/feedbacks/student-4.jpg"
-
+import { AiOutlinePlayCircle } from "react-icons/ai";
+import { feedbacks } from "./data/dataFeedback";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa6";
 
 function Feedback (){
 
-    const feedbacks = [
-        
-        {
-            image: <Image src={student1} />,
-            comment: "Lorem Ipsum is simply dummy text of the printi.",
-            autor: "Bruno Oliveira"
-        },
-        {
-            image: <Image src={student2}/>,
-            comment: "Lorem Ipsum is simply dummy text of the printi.",
-            autor: "Bruno Oliveira"
-        },
-        {
-            image: <Image src={student3} />,
-            comment: "Lorem Ipsum is simply dummy text of the printi.",
-            autor: "Bruno Oliveira"
-        },
-        {
-            image: <Image src={student4} />,
-            comment: "Lorem Ipsum is simply dummy text of the printi.",
-            autor: "Bruno Oliveira"
+    const [feedbackEmphasis, setFeedbackEmphasis] = useState(feedbacks[0])
+
+    const renderStars = (score) => {
+        const stars = [];
+        const maxScore = 5; // Define a pontuação máxima possível (você pode ajustar conforme necessário)
+        for (let i = 0; i < maxScore; i++) {
+            // Verifica se o índice atual é menor que o score, e adiciona uma estrela preenchida.
+            // Caso contrário, adiciona uma estrela vazia.
+            if (i < score) {
+                stars.push(<FaStar key={i} />);
+            } else {
+                stars.push(<FaStar key={i} style={{ opacity: 0.5 }} />); // Estrela vazia com opacidade reduzida
+            }
         }
-    ]
+        return stars;
+    };
 
     return (
         <section className="feedback-section">
-            <div className="feedback-title"> 
+            <div className="feedback-header"> 
                 <h2> O QUE NOSSOS <span> ALUNOS </span> DIZEM SOBRE NÓS</h2>
+                <p>
+                Aqui na nossa seção de depoimentos, você encontrará experiências autênticas compartilhadas por aqueles que já fazem parte da família.
+                </p>
             </div>
 
-            <div className="feedback-content">
-                {feedbacks.map((feedback, index) => (
-                    <div key={index} className="feedback-item"> 
-                        <div className="feedback-image"> 
-                            {feedback.image}
-                        </div>
-                        <div className="feedback-details"> 
-                            <div> {feedback.comment} </div>
-                            <div> {feedback.autor} </div>
-                            <div>
-                                <BsYoutube color="red" /> 
-                                Assista no YouTube
-                            </div>
-                        </div>
+            <div className="feedback-content">  
+
+                <div className="feedback-emphasis">
+                    {feedbackEmphasis.image}
+                    <div className="icon-play">
+                        <AiOutlinePlayCircle />
                     </div>
-                ))}
+                </div>
+
+                <div> 
+                    <div className="feedback-info">
+                        <h3 className="feedback-autor"> {feedbackEmphasis.autor} </h3>
+                        <h4 className="feedback-course"> {feedbackEmphasis.course}  </h4>
+                        <div className="feedback-stars">
+                            {renderStars(feedbackEmphasis.score)}
+                        </div>
+                        <h4 className="feedback-title"> {feedbackEmphasis.title} </h4>
+                        <p className="feedback-comment"> {feedbackEmphasis.comment} </p>
+                    </div> 
+                    <div className="other-feedbacks">
+                        {feedbacks.map(item => (
+                            <div className="feedback-item" onClick={() => setFeedbackEmphasis(item)}>
+                                <div className="box-image">
+                                    {item.image}
+                                </div>
+                                <div className="box-autor">
+                                    <h5> {item.autor} </h5>
+                                    <h6> {item.course} </h6>
+                                </div>
+                            </div>
+                        ))}
+                    </div> 
+                </div>
                 
 
                 
---
+++ website/src/pages/Feedback/data/dataFeedback.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import student1 from "../../../assets/feedbacks/student-1.jpg"
+import student2 from "../../../assets/feedbacks/student-2.jpg"
+import student3 from "../../../assets/feedbacks/student-3.jpg"
+import student4 from "../../../assets/feedbacks/student-4.jpg"
+
+
+export const feedbacks = [
+    {
+        image: <Image src={student1} />,
+        autor: "Juliana Lopes",
+        course: 'Design Gráfico',
+        score: 4,
+        title: 'Excelente curso!',
+        comment: "Gostei muito do curso de Design Gráfico. Os materiais didáticos são excelentes e os instrutores são muito experientes. Recomendo fortemente!",
+    },
+    {
+        image: <Image src={student2} />,
+        autor: "Bruno Oliveira",
+        course: 'Front-end Development',
+        score: 5,
+        title: 'Ótimo conteúdo',
+        comment: "O curso de Front-end Development foi extremamente útil para minha carreira. Aprendi muitas técnicas novas e agora me sinto confiante para trabalhar em projetos mais complexos.",
+    },
+    {
+        image: <Image src={student3} />,
+        autor: "Ana Silva",
+        course: 'Data Science',
+        score: 3,
+        title: 'Recomendo totalmente!',
+        comment: "O curso de Data Science superou minhas expectativas. Os instrutores são muito competentes e os projetos práticos foram extremamente enriquecedores.",
+    },
+    {
+        image: <Image src={student4} />,
+        autor: "Ricardo Mendes",
+        course: 'Cloud Computing',
+        score: 4,
+        title: 'Muito informativo',
+        comment: "O curso de Cloud Computing me deu uma compreensão sólida dos conceitos fundamentais e das práticas recomendadas na nuvem. Altamente recomendado para iniciantes!",
+    },
+    {
+        image: <Image src={student1} />,
+        autor: "Luana Santos",
+        course: 'Introdução à Programação',
+        score: 5,
+        title: 'Fantástico!',
+        comment: "Este curso de Introdução à Programação é simplesmente fantástico. Aprendi muito e agora estou ansiosa para me aprofundar mais nesse campo.",
+    },
+    {
+        image: <Image src={student2} />,
+        autor: "Pedro Martins",
+        course: 'Machine Learning',
+        score: 5,
+        title: 'Impressionante!',
+        comment: "O curso de Machine Learning superou todas as minhas expectativas. Os casos de estudo práticos foram muito úteis para entender os conceitos mais complexos.",
+    }
+];
