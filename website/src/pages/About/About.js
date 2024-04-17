33cc5a8c06dabbd8fc39688d4e76c024a98b4c6a
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-17 09:20:39 -0300

    Refatorando a seção about

--- website/src/pages/About/About.js
+++ website/src/pages/About/About.js
@@ -1,42 +1,36 @@
 import "./About.css"
-import assetPeople from "../../assets/about/asset-people.jpg"
 import Image from "next/image"
+import assetSectionAbout from "../../assets/about/asset-section-about.jpg"
+import { AiOutlinePlayCircle } from "react-icons/ai"
+import { FaArrowTrendUp } from "react-icons/fa6";
+
 function About(){
     return (
         <section className="about-section">
-            <div className="about-title">
-                <h2>QUEM NÓS SOMOS ?</h2>
-                <div> </div>
-            </div>
-
-            <div className="about-content"> 
-                <div className="about-text">
-                    
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type
-                         specimen book. It has survived not only five centuries, but also the leap into
-                         electronic typesetting, remaining essentially unchanged. It was popularised in
-                         the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                         and more recently with desktop publishing software like Aldus PageMaker
-                         including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is 
-                         not simply random text. It has roots in a piece of classical Latin literature 
-                         from 45 BC, making it over 2000 years old. 
-                    </p>
-
-                    <p>
-                        <strong> Ricardo Alencar </strong><br />
-                        <span>Diretor</span>
-                    </p>
-                    <div className="aspa-left"> “ </div> 
-                    <div className="aspa-right"> ”</div> 
+            <div className="about-section-box-left">
+                <div className="about-title">
+                    <h2>DESCUBRA <span> QUEM NÓS </span>SOMOS ?</h2>
                 </div>
 
-                <div className="about-image"> 
-                    <Image src={assetPeople}/>
-                    <div className="background-1"></div>
-                    <div className="background-2"></div>
+                <div className="about-text"> 
+                    <p>Bem-vindo à TechInnovate Specialist Academy, sua plataforma de excelência para o desenvolvimento de especialistas em tecnologia e inovação! Na TechInnovate, estamos comprometidos em capacitar indivíduos talentosos com as habilidades e conhecimentos necessários para liderar na vanguarda da revolução tecnológica.</p>
+
+                    <p>Em um mundo cada vez mais digital e dinâmico, a demanda por especialistas em tecnologia e inovação nunca foi tão alta. Na TechInnovate, reconhecemos a importância de oferecer uma educação de classe mundial que não apenas atenda, mas ultrapasse as expectativas do mercado. Nossos cursos são cuidadosamente projetados e ministrados por especialistas do setor, garantindo que os alunos adquiram habilidades práticas e conhecimentos atualizados que são diretamente aplicáveis ​​ao mundo real.
+                    </p>
+
+                    <div className="box-button-know-more">
+                        <button> Saiba mais <FaArrowTrendUp /> </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="about-section-box-right"> 
+
+                <div>
+                    {/* <Image src={assetSectionAbout} /> */}
+                    <div className="play-video-about">
+                        <AiOutlinePlayCircle />
+                    </div>               
                 </div>
             </div>
             
